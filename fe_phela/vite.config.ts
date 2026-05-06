1b86c6ac2d07f637e32ef6585f99beb0622c12b9
QDat18 <161273032+QDat18@users.noreply.github.com>
2026-05-06 21:56:14 +0700

    Update vite.config.ts

--- fe_phela/vite.config.ts
+++ fe_phela/vite.config.ts
@@ -1,22 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/",
+export default {
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   build: {
-    outDir: "build/client",
-    emptyOutDir: true,
+    outDir: "dist",
   },
-  preview: {
-    port: 3000,
-    host: true,
-    strictPort: false,
-    allowedHosts: true
-  },
-  define: {
-    global: 'window',
-  },
-});
+};
